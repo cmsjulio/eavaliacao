b6c7f81097ffcc951e04512c59e5b2558e382dcc
cmsjulio <cms.julio1@gmail.com>
2022-02-20 19:02:36 -0300

    Teste salvarDisciplina, do POST em /disciplina, implementado

--- src/test/java/br/mil/ccarj/baseapi/DisciplinaTest.java
+++ src/test/java/br/mil/ccarj/baseapi/DisciplinaTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DisciplinaTest {
@@ -20,12 +21,29 @@ public class DisciplinaTest {
 
     @Test
     @Sql("/test.sql")
-    public void getDisciplinaByIdTest(){
+    public void getDisciplinaByIdTest() {
 
         ResponseEntity<Disciplina> response = testRestTemplate.getForEntity("/disciplina/1", Disciplina.class);
 
         assertEquals(1, response.getBody().getId());
         assertEquals("Matemática", response.getBody().getNome());
+
+    }
+
+    @Test
+    public void salvarDisciplina() {
+
+        Disciplina disciplina = new Disciplina();
+
+        disciplina.setNome("História");
+
+        HttpEntity<Disciplina> request = new HttpEntity<>(disciplina);
+
+        ResponseEntity<Disciplina> response = testRestTemplate.postForEntity("/disciplina", request, Disciplina.class);
+
+        assertNotNull(response.getBody().getId());
+
+        assertEquals("História", response.getBody().getNome());
 
     }
 
